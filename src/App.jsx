785a452917aa2abbe8e37e19d8adf4857f2a8d17
juliustrn <thuanthien2004@gmail.com>
2026-07-04 01:32:15 -0500

    Implemented the Admin Dashboard and Service Management Screen
    Redo the open/close queue feature
    Add "Add services" and "Find Services" features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { NotificationProvider } from './context/NotificationContext';
 import LoginPage from './pages/LoginPage';
-import DashboardPage from './pages/DashboardPage';
+import DashboardPage from './pages/admin_pages/DashboardPage';
 import './index.css';
 
 export default function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
 
   return (
     <NotificationProvider>
