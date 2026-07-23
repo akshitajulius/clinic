81900ee7cb81cd162811a5fd9027c827e89527bb
Akshita Julius <akshijulius@gmail.com>
2026-07-22 20:35:00 -0500

    Add wait-time, notifications, and history modules

---
+++ src/backend/api.js
@@ -0,0 +1,5 @@
+export * from "./modules/services.js";
+export * from "./modules/queue.js";
+export * from "./modules/waitTime.js";
+export * from "./modules/notifications.js";
+export * from "./modules/history.js";
